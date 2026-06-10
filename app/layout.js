104c1e1226acc821c3943e30f44031da5fac635a
Sahel Qureshi <sahelqureshi0089@gmail.com>
2026-06-10 22:14:42 +0530

    design modified

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import ScrollSmootherProvider from "@/components/ScrollSmootherProvider";
 import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Header/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,12 +26,12 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#040120]  `}
         suppressHydrationWarning={true}
       >
-        <ScrollSmootherProvider>
+<Navbar/>
          
           {children}
           <Footer />
           
-        </ScrollSmootherProvider>
+      
       </body>
     </html>
   );
